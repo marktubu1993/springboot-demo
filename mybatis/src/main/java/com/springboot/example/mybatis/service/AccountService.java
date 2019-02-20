9e0deee23c9829f10a6d4c3f7d18ea9c89cd27bb
Liu Xu <liuxu@eastedu.com>
2019-02-20 15:19:22 +0800

    transactional 事务

--- mybatis/src/main/java/com/springboot/example/mybatis/service/AccountService.java
+++ mybatis/src/main/java/com/springboot/example/mybatis/service/AccountService.java
@@ -4,6 +4,7 @@ import com.springboot.example.mybatis.dao.AccountMapper;
 import com.springboot.example.mybatis.domain.Account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -19,7 +20,9 @@ public class AccountService {
         return accountMapper.add(name, money);
     }
 
+    @Transactional(rollbackFor = Exception.class)
     public int update(String name, double money, int id) {
+        accountMapper.update("test",290,1);
         return accountMapper.update(name, money, id);
     }
 
