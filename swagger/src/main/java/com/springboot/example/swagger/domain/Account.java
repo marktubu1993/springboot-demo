647fd5453bd94f3cba4e584f8767d68bb47d01f3
Liu Xu <liuxu@eastedu.com>
2019-02-21 14:43:54 +0800

    swagger

---
+++ swagger/src/main/java/com/springboot/example/swagger/domain/Account.java
@@ -0,0 +1,13 @@
+package com.springboot.example.swagger.domain;
+
+import lombok.Data;
+
+/**
+ * @author mark-liu
+ */
+@Data
+public class Account {
+    private Long id;
+    private String name;
+    private String money;
+}
